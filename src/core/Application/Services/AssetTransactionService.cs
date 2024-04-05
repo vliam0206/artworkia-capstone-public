b11f45f4391b5ee16182464692ed298cc53db4a7
『Zenith』 <phuhuynh923@gmail.com>
2024-04-05 10:25:47 +0700

    [Phu][fix api] (#91)
    * Update AssetTransactionService.cs
    
    * Update AssetTransactionService.cs

--- src/core/Application/Services/AssetTransactionService.cs
+++ src/core/Application/Services/AssetTransactionService.cs
@@ -30,44 +30,57 @@ public class AssetTransactionService : IAssetTransactionService
             .GetSingleByConditionAsync(x => x.CreatedBy == _claimService.GetCurrentUserId
             && x.AssetId == assetTransactionModel.AssetId);
         if (assetTransaction is not null)
-            throw new Exception("You already have this asset in your library");
+            throw new Exception("Bạn đã mua tài nguyên này.");
 
         // Check if asset is not found or deleted
         var asset = await _unitOfWork.AssetRepository.GetAssetAndItsCreatorAsync(assetTransactionModel.AssetId);
         if (asset is null)
-            throw new NullReferenceException("Asset not found");
+            throw new NullReferenceException("Tài nguyên không tìm thấy.");
         if (asset.DeletedOn != null)
-            throw new Exception("Asset is deleted");
+            throw new Exception("Tài nguyên đã bị xóa");
 
         // Check if user has enough money to buy this asset
         Guid accountId = _claimService.GetCurrentUserId ?? default;
         Wallet? wallet = await _unitOfWork.WalletRepository.GetSingleByConditionAsync(x => x.AccountId == accountId);
         Wallet? sellerWallet = await _unitOfWork.WalletRepository.GetSingleByConditionAsync(x => x.AccountId == asset.Artwork.CreatedBy);
 
-        if (wallet is null || sellerWallet is null)
+        if (wallet is null)
         {
-            throw new NullReferenceException("Wallet not found");
+            throw new NullReferenceException("Không tìm thấy ví của người mua.");
+        }
+        if (sellerWallet is null)
+        {
+            throw new NullReferenceException("Không tìm thấy ví của người bán.");
         }
         if (wallet.Balance < asset.Price)
-            throw new Exception("You don't have enough money to buy this asset");
+            throw new Exception("Bạn không đủ tiền để mua tài nguyên này");
 
         wallet.Balance -= asset.Price;
         sellerWallet.Balance += asset.Price;
 
-        TransactionHistory newAssetTransaction = new TransactionHistory()
+        TransactionHistory newAssetTransactionForBuyer = new TransactionHistory()
         {
             CreatedBy = accountId,
             AssetId = assetTransactionModel.AssetId,
-            Detail = $"Bought {asset.AssetTitle} for {asset.Price} coins successfully",
+            Detail = $"Bạn đã mua tài nguyên \"{asset.AssetTitle}\"",
+            Price = -asset.Price,
+            TransactionStatus = TransactionStatusEnum.Success,
+        };
+        TransactionHistory newAssetTransactionForSeller = new TransactionHistory()
+        {
+            AssetId = assetTransactionModel.AssetId,
+            Detail = $"Người dùng \"{_claimService.GetCurrentUserName}\" đã mua tài nguyên \"{asset.AssetTitle}\"",
             Price = asset.Price,
             TransactionStatus = TransactionStatusEnum.Success,
         };
 
-        await _unitOfWork.TransactionHistoryRepository.AddAsync(newAssetTransaction);
+        await _unitOfWork.TransactionHistoryRepository.AddAsync(newAssetTransactionForBuyer);
+        await _unitOfWork.TransactionHistoryRepository.AddAsync(newAssetTransactionForSeller);
+        newAssetTransactionForSeller.CreatedBy = asset.Artwork.CreatedBy;
         _unitOfWork.WalletRepository.Update(wallet);
         _unitOfWork.WalletRepository.Update(sellerWallet);
         await _unitOfWork.SaveChangesAsync();
-        var result = _mapper.Map<AssetTransactionVM>(newAssetTransaction);
+        var result = _mapper.Map<AssetTransactionVM>(newAssetTransactionForBuyer);
         return result;
     }
 
@@ -92,7 +105,7 @@ public class AssetTransactionService : IAssetTransactionService
     {
         var assetTransaction = await _unitOfWork.TransactionHistoryRepository.GetByIdAsync(assetTransactionId);
         if (assetTransaction is null)
-            throw new NullReferenceException("Asset transaction not found");
+            throw new NullReferenceException("Giao dịch tài nguyên không tìm thấy.");
         var result = _mapper.Map<AssetTransactionVM>(assetTransaction);
         return result;
     }
